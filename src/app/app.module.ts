4ecdb03401372874f404c717f9974cb8985a3813
gverissi <greg.verissimo@gmail.com>
2021-05-02 15:07:15 +0200

    Add home component and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent
   ],
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
