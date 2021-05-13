cf8a7d14715faaf19a3eb6e1cd5acd49935286cd
gverissi <greg.verissimo@gmail.com>
2021-05-14 00:20:02 +0200

    Add notification in page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {AuthService} from './services/auth/auth.service';
+import {CustomerDaoService} from './services/customer-dao/customer-dao.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
   title = 'MyChatRoom';
+  subscriptionAuth: Subscription;
+  subscriptionCustomer: Subscription;
+  favIcon: HTMLLinkElement = document.querySelector('#appIcon');
+
+  public constructor(private titleService: Title, private authService: AuthService, private customerDao: CustomerDaoService) {
+  }
+
+  ngOnInit(): void {
+    this.subscriptionAuth = this.authService.getAuthState().subscribe(user => {
+      if (user) {
+        const customerName = localStorage.getItem('customerName');
+        this.subscriptionCustomer = this.customerDao.findByName(customerName).subscribe(customer => {
+          const nbUnreadMessages = customer.newMessages.length;
+          if (nbUnreadMessages > 0) {
+            this.titleService.setTitle('(' + nbUnreadMessages + ') ' + this.title);
+            this.favIcon.href = 'favicon-red.ico';
+          } else {
+            this.titleService.setTitle(this.title);
+            this.favIcon.href = 'favicon.ico';
+          }
+        });
+      } else {
+        if (this.subscriptionCustomer) {
+          this.subscriptionCustomer.unsubscribe();
+        }
+        this.titleService.setTitle(this.title);
+        this.favIcon.href = 'favicon.ico';
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionCustomer.unsubscribe();
+    this.subscriptionAuth.unsubscribe();
+  }
+
 }
